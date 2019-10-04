f5bd05573f1caa0967b4d3d574db04d113405428
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-04 08:29:07 -0700

    got a run time script up and running

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from "react-dom";
-import Main from './components/Main.js'
+import Main from './components/Main/Main.js'
 
 ReactDOM.render(<Main />, document.getElementById('root'));
