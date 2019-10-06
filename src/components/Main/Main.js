d9eb99e3da701bdfde9bfdee774fb17d5d1ecfc7
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-05 18:25:44 -0700

    added a new loa.js looking nice

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,26 @@
 import React from 'react'
 
+//** PAGE COMPONENTS  **//
+import NavigationBar from '../Navbar/Navbar.js'
+import Title from '../Title/Title.js'
+import AboutMe from '../AboutMe/AboutMe.js'
+import Skills from '../Skills/Skills.js'
+import Portfolio from '../Portfolio/Portfolio.js'
+import ContactMe from '../ContactMe/ContactMe.js'
+import Footer from '../Footer/Footer.js'
+
 
 export default function Main() {
     return (
         <div>
-            <p>test</p>
+           <NavigationBar />
+           <Title />
+           <AboutMe />
+           <Skills />
+           <Portfolio />
+           <ContactMe />
+           <Footer />
+
         </div>
     )
 }
---
+++ src/components/Title/Title.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Bounce from 'react-reveal/Bounce';
+import styled from 'styled-components';
+import codingbackground from "./Images/codingbackground.jpg"
+
+export default function Title() {
+
+    const Wrapper = styled.h1`
+    padding: 10em;
+    background-image: url(${codingbackground});
+    background-size: cover;
+    background-position: center;
+    
+  `;
+
+  const Title = styled.h1`
+  text-align: center;
+  color: white; 
+  `
+
+  const TitleSub = styled.h2`
+  text-align: center;
+  color: white; 
+  `
+    return (
+    
+    <Wrapper>
+        {/* <Bounce Top><img src={AstroBearLogo} className=" AstroBearLogo"></img></Bounce> */}
+        <Bounce Top><Title>Brandon Harris</Title></Bounce>
+        
+        <Bounce Bottom><TitleSub>Full Stack Web Developer</TitleSub></Bounce>
+       
+      </Wrapper>
+      
+        
+    )
+}
