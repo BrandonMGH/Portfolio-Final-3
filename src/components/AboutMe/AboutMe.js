99f0a1b80ee8014f0c04fc4f57e043487069d34c
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-05 22:54:16 -0700

    added the basic jsx and styling for AboutMe component

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,18 +5,34 @@ import AstroBearLogoInverse from './AstroBearLogoInverse.png'
 
 export default function AboutMe() {
 
-    const SubjectHeader = styled.h1`
+    const AboutMeHeader = styled.h1`
     text-align: center;
     padding: 50px; 
     color: black; 
     margin-top: 25px;
     
   `;
+
+    const AboutMeParagraph = styled.p`
+  text-align: center;
+  padding: 50px; 
+  color: black; 
+  margin-top: 25px;
+  `
+
+    const AboutMeInfo = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 50%;
+  height: 250px;
+  width: 250px; `
+
     return (
         <div>
-            <SubjectHeader>- ABOUT ME - </SubjectHeader>
-                <img src={AstroBearLogoInverse} className="AboutMeImg" alt="me" />
-                <p className="AboutMe">I am a Full Stack Web Developer with a certificate of completion from the UC San Diego Bootcamp Extension Course.  I specialize in the creation of dynamic websites that emphasize ease of use and timely responsiveness</p>
+            <AboutMeHeader>- ABOUT ME - </AboutMeHeader>
+            <AboutMeInfo src={AstroBearLogoInverse} />
+            <AboutMeParagraph>I am a Full Stack Web Developer with a certificate of completion from the UC San Diego Bootcamp Extension Course.  I specialize in the creation of dynamic websites that emphasize ease of use and timely responsiveness</AboutMeParagraph>
         </div>
     )
 }
