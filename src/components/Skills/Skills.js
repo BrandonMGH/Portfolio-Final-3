cab887aca8d4f5e919d7c8287c426debc1396238
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-08 12:47:26 -0700

    was able to get the skills object items to align using grid system.  Still trying to get them to display 3 in a row

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,16 +8,22 @@ export default function Skills() {
     const SkillsTitle = styled.h1`
     text-align:center; 
     `
+    const SkillsObjContainer = styled.section`
+    display: grid; 
+    justify-content: center; 
+ 
+     
+    `
 
 
     return (
         <div>
             <SkillsTitle> -SKILLS- </SkillsTitle>
+            <SkillsObjContainer>
             {skillsObject.map(properties => (
-                <img key={properties.key} src={properties.image} />
-                
+               <img key={properties.key} src={properties.image} />
             ))}
-     
+           </SkillsObjContainer>
 
         </div>
     )
