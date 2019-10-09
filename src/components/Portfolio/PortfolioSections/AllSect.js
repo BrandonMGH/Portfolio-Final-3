0ac0aa3c0a66f628e74cbce77cf917f7d3a29317
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-09 13:53:07 -0700

    applied some basic grid styling to the sectionObject element inside of the AllSect.js file

--- src/components/Portfolio/PortfolioSections/AllSect.js
+++ src/components/Portfolio/PortfolioSections/AllSect.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import styled from 'styled-components';
 import sectionObject from './SectionObject/SectionObject.js';
 
+//** Images **//
+
 export default function AllSect() {
-    console.log(sectionObject.All)
+
+    const PortfolioTileWrapper = styled.section`
+    display: grid; 
+    grid-template-columns: repeat(3, 1fr); 
+    grid-gap: 1rem;
+    place-items: center; 
+    text-align: center; 
+    `
+
+
     return (
-        <div>
+        <PortfolioTileWrapper>
             {sectionObject.All.map(properties => (
-              <span key={properties.keyValue}>
-                  <h1 >{properties.projectTitle}</h1>
-              </span>
-              
+                <span key={properties.keyValue}>
+                    <h3 >{properties.projectTitle}</h3>
+                    <p>{properties.projectDescription}</p>
+                    <p><b>Project Type:</b> {properties.projectType}</p>
+                    <p><b>My Responsibility:</b></p>
+
+                </span>
+
             ))}
-        </div>
+        </PortfolioTileWrapper>
     )
 }
