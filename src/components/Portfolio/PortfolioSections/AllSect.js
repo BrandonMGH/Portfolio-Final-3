409da5a2140cbd663496472a3d4f88c6670f5660
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-09 10:57:56 -0700

    created a sectionObject.js containing the sectionObject object which will contain all the info for all the portfolio projects.  Also was able to map through the option and get the info to display on one of the section.js' pages

--- src/components/Portfolio/PortfolioSections/AllSect.js
+++ src/components/Portfolio/PortfolioSections/AllSect.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import sectionObject from './SectionObject/SectionObject.js';
 
 export default function AllSect() {
+    console.log(sectionObject.All)
     return (
         <div>
-            <p>All Section</p>
+            {sectionObject.All.map(properties => (
+              <span key={properties.keyValue}>
+                  <h1 >{properties.projectTitle}</h1>
+              </span>
+              
+            ))}
         </div>
     )
 }
---
+++ src/components/Portfolio/PortfolioSections/SectionObject/SectionObject.js
@@ -0,0 +1,43 @@
+
+// // ** Javascript Portfolio Images ** // 
+
+// import PsyButImg from "./Images/PsyButImg.jpg"
+// import Link from './Images/Link.jpg'
+// import MontyPythonHolyGrail from './Images/MontyPythonHolyGrail.png'
+// import DarthVader from './Images/DarthVader.jpg'
+// import train from './Images/train.jpg'
+// import GameofThronesWallpaper from './Images/GameofThronesWallpaper.jpg'
+
+// // ** Node Portfolio Images ** // 
+
+// import botimage2 from './Images/botimage2.jpg'
+// import wordguesser from './Images/wordguesser.jpg'
+// import OnlineShoppingIcon from './Images/OnlineShoppingIcon.png'
+// import bestfriends from './Images/bestfriends.jpg'
+// import hamburger from './Images/hamburger.jpg'
+// import codespot from './Images/codespot.PNG'
+
+// // ** React Portfolio Images ** // 
+
+// import newspapers from './Images/newspapers.jpg'
+// import spongebob from './Images/spongebob.jpg'
+// import photographer from './Images/photographer.jpg'
+
+
+const sectionObject = {
+All: [
+    {
+        keyValue: 1,
+        projectTitle: "Psychic Game",
+        projectDescription:"A Psychic Game where a user attempts to guess a hidden letter.  The user has a limited number of chances to guess the letter.  If they guess it before their guesses run out, they win, otherwise, they lose.",
+        projectType: "Solo Project",
+        projectResponsibility: "Built the project in its entirety"
+
+    }
+]
+
+
+
+}
+
+export default sectionObject
