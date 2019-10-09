8661e451cd950615e6cfe3c7cd3bc9a6ae47fa2d
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-08 23:29:16 -0700

    added portfolio tiles to be used as future clickable buttons to display different portfolio projects.  Used grid system to align and make responsive

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import styled from 'styled-components';
 
 export default function Portfolio() {
+
+    const PortfolioWrapper = styled.section`
+    display: grid; 
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 1rem;
+    place-items:center; 
+  `
+   const PortfolioTile = styled.section`
+    border: 3px solid black; 
+    width:100px; 
+   `
+   const TileJavascript = styled(PortfolioTile)`
+   background-color: orange; 
+   `
+   const TileNode = styled(PortfolioTile)`
+   background-color: Green;
+   `
+   const TileReact = styled(PortfolioTile)`
+   background-color: Blue; 
+   `
+   const TileAll = styled(PortfolioTile)`
+   background-color: Red; 
+   `
+
     return (
-        <div>
-            <h1>Portfolio</h1>
-        </div>
+        <PortfolioWrapper>
+            <TileJavascript>
+                test
+            </TileJavascript>
+            <TileNode>
+                test
+            </TileNode>
+            <TileReact>
+                test
+            </TileReact>
+            <TileAll>
+                test
+            </TileAll>
+        </PortfolioWrapper>
     )
 }
