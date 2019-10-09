254fd916057b7bd87455b18114c6e5ed6bb6c835
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-09 10:59:33 -0700

    added projectButtonLink and githubButtonLink properties to section object

--- src/components/Portfolio/PortfolioSections/SectionObject/SectionObject.js
+++ src/components/Portfolio/PortfolioSections/SectionObject/SectionObject.js
@@ -31,7 +31,9 @@ All: [
         projectTitle: "Psychic Game",
         projectDescription:"A Psychic Game where a user attempts to guess a hidden letter.  The user has a limited number of chances to guess the letter.  If they guess it before their guesses run out, they win, otherwise, they lose.",
         projectType: "Solo Project",
-        projectResponsibility: "Built the project in its entirety"
+        projectResponsibility: "Built the project in its entirety",
+        projectButtonLink: "TBD",
+        githubButtonLink: "TBD"
 
     }
 ]
