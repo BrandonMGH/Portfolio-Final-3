4401d25ff0dbf1eb5565817be3ca50c1394a8b12
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-09 14:50:43 -0700

    created a PortfolioTileTile styled component that was able to take in an imageInput prop and use it to cycle through images from the SectionObject.js in order to use them as different background images for each individua portfolio tile

--- src/components/Portfolio/PortfolioSections/AllSect.js
+++ src/components/Portfolio/PortfolioSections/AllSect.js
@@ -8,10 +8,23 @@ export default function AllSect() {
 
     const PortfolioTileWrapper = styled.section`
     display: grid; 
-    grid-template-columns: repeat(3, 1fr); 
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     grid-gap: 1rem;
     place-items: center; 
-    text-align: center; 
+    text-align: center;  
+    `
+
+    const PortfolioTileTitle = styled.h3`
+    background-image: url(${props => props.imageInput});
+    background-size: cover;
+    background-position: center;
+    height: 250px; 
+    display:flex;
+    justify-content: center; 
+    align-items: center; 
+    margin-top: 1em; 
+    color: white; 
+    
     `
 
 
@@ -19,10 +32,12 @@ export default function AllSect() {
         <PortfolioTileWrapper>
             {sectionObject.All.map(properties => (
                 <span key={properties.keyValue}>
-                    <h3 >{properties.projectTitle}</h3>
+                    <PortfolioTileTitle imageInput={properties.projectImg} >{properties.projectTitle}</PortfolioTileTitle>
                     <p>{properties.projectDescription}</p>
                     <p><b>Project Type:</b> {properties.projectType}</p>
-                    <p><b>My Responsibility:</b></p>
+                    <p><b>My Responsibility:</b>{properties.projectResponsibility}</p>
+                    <button>Project Link</button>
+                    <button>GitHub Link</button>
 
                 </span>
 
--- src/components/Portfolio/PortfolioSections/SectionObject/SectionObject.js
+++ src/components/Portfolio/PortfolioSections/SectionObject/SectionObject.js
@@ -1,16 +1,54 @@
-
+import PsyButImg from "./Images/PsyButImg.jpg"
+import Link from './Images/Link.jpg'
+import MontyPythonHolyGrail from './Images/MontyPythonHolyGrail.png'
+// import DarthVader from './Images/DarthVader.jpg'
+// import train from './Images/train.jpg'
+// import GameofThronesWallpaper from './Images/GameofThronesWallpaper.jpg'
+// import botimage2 from './Images/botimage2.jpg'
+// import wordguesser from './Images/wordguesser.jpg'
+// import OnlineShoppingIcon from './Images/OnlineShoppingIcon.png'
+// import bestfriends from './Images/bestfriends.jpg'
+// import hamburger from './Images/hamburger.jpg'
+// import codespot from './Images/codespot.PNG'
+// import newspapers from './Images/newspapers.jpg'
+// import spongebob from './Images/spongebob.jpg'
+// import photographer from './Images/photographer.jpg'
 
 const sectionObject = {
 All: [
     {
         keyValue: 1,
         projectTitle: "Psychic Game",
+        projectImg: PsyButImg,
         projectDescription:"A Psychic Game where a user attempts to guess a hidden letter.  The user has a limited number of chances to guess the letter.  If they guess it before their guesses run out, they win, otherwise, they lose.",
         projectType: "Solo Project",
         projectResponsibility: "Built the project in its entirety",
         projectButtonLink: "*",
-        githubButtonLink: "*"
+        githubButtonLink: "*",
+        color: "red"
 
+    },
+    {
+        keyValue: 2,
+        projectTitle: "Psychic Game",
+        projectImg: Link,
+        projectDescription:"A Psychic Game where a user attempts to guess a hidden letter.  The user has a limited number of chances to guess the letter.  If they guess it before their guesses run out, they win, otherwise, they lose.",
+        projectType: "Solo Project",
+        projectResponsibility: "Built the project in its entirety",
+        projectButtonLink: "*",
+        githubButtonLink: "*",
+        color: "green"
+    },
+    {
+        keyValue: 3,
+        projectTitle: "Psychic Game",
+        projectImg: MontyPythonHolyGrail,
+        projectDescription:"A Psychic Game where a user attempts to guess a hidden letter.  The user has a limited number of chances to guess the letter.  If they guess it before their guesses run out, they win, otherwise, they lose.",
+        projectType: "Solo Project",
+        projectResponsibility: "Built the project in its entirety",
+        projectButtonLink: "*",
+        githubButtonLink: "*",
+        color: "blue"
     }
 ]
 
