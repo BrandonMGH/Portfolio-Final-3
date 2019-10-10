2966128fd52a4315940809c96afa6e215c3f2ea8
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-10 13:07:12 -0700

    working on the styling and funcitonality of the portfolio tile buttons

--- src/components/Portfolio/PortfolioSections/AllSect.js
+++ src/components/Portfolio/PortfolioSections/AllSect.js
@@ -11,17 +11,16 @@ export default function AllSect() {
     grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
     grid-gap: 2rem;
     place-items: center; 
-    text-align: center; 
+    padding-bottom: 50px; 
     `
     const PortfolioTileElement = styled.section`
-  
-    border-radius: 10px;
-    box-shadow: 10px 10px 10px 0 rgba(0,0,0,0.2);
+    border-radius: 50px;
     transition: 0.3s;
     :hover {
-        box-shadow: 20px 20px 20px 0 rgba(0,0,0,0.2);
+        box-shadow: 15px 15px 15px 15px rgba(0,0,0,0.2);
       }
-    padding: 1em 1em 1em 1em
+    padding: 2em 2em 2em 2em;
+
     `
 
     const PortfolioTileImage = styled.section`
@@ -30,21 +29,13 @@ export default function AllSect() {
     background-position: center;
     height: 250px; 
     display:flex;
-    justify-content: center; 
-    align-items: center; 
+    place-items: center; 
     margin-top: 1em;  
     border: 3px solid black;
     margin-bottom: 20px; 
-    
     `
-    
-
-
     return (
-      
-    
         <PortfolioTileWrapper>
-             
             {sectionObject.All.map(properties => (
                 <PortfolioTileElement key={properties.keyValue}>
                     <PortfolioTileImage imageInput={properties.projectImg} ></PortfolioTileImage>
@@ -54,9 +45,7 @@ export default function AllSect() {
                     <p><b>My Responsibility:</b>{properties.projectResponsibility}</p>
                     <button>Project Link</button>
                     <button>GitHub Link</button>
-
                 </PortfolioTileElement>
-
             ))}
         </PortfolioTileWrapper>
     
