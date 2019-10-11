ebc367f63df525285e02cba8a08f0d3178651c11
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-11 13:39:50 -0700

    finished updating the SectionObject so that all the porndering all the proper tiles

--- src/components/Portfolio/PortfolioSections/SectionObject/SectionObject.js
+++ src/components/Portfolio/PortfolioSections/SectionObject/SectionObject.js
@@ -82,48 +82,37 @@ const sectionObject = {
     Node: [
         {
             keyValue: 1,
-            projectTitle: "NewsScraper",
-            projectImg: newspapers,
-            projectDescription: "an app designed to scrape elements from the dom in order to present users with up to date news articles from the news website Quillette ",
+            projectTitle: "Liri-bot",
+            projectImg: botimage2,
+            projectDescription: "a node command line app that allows users to type choose from a couple different options and make queries to APIs associated with the option chosen.  Data returned from the API will be shown in the terminal. ",
             projectType: "Solo Project",
             projectResponsibility: "Built the project in its entirety",
-            projectButtonLink: "https://quillette-app-scrap.herokuapp.com/",
-            githubButtonLink: "https://github.com/BrandonMGH/NewsScraper",
+            projectButtonLink:  "https://github.com/BrandonMGH/liri-node-ap",
+            githubButtonLink: "https://github.com/BrandonMGH/liri-node-ap"
 
         },
         {
             keyValue: 2,
-            projectTitle: "Clicky-Game",
-            projectImg: DragonBallZWallpaper,
-            projectDescription: "An app that allows users to click on character cards to earn points.  Users win when they click on all the unique character cards without click the same one twice.",
+            projectTitle: "Constructor Word Guesser",
+            projectImg: wordguesser,
+            projectDescription: "a command line app that asks users to guess a sci movie by inputting letters into the terminal. ",
             projectType: "Solo Project",
             projectResponsibility: "Built the project in its entirety",
-            projectButtonLink: "https://clicky-game-reactjs-app.herokuapp.com/",
-            githubButtonLink: "https://github.com/BrandonMGH/Clicky-Game",
+            projectButtonLink: "https://github.com/BrandonMGH/Constructor-Word-Guess",
+            githubButtonLink: "https://github.com/BrandonMGH/Constructor-Word-Guess",
         },
         {
             keyValue: 3,
-            projectTitle: "SparkItechs",
-            projectImg: photographer,
-            projectDescription: "A personalized website designed to represent an individual's personal portfolio website",
-            projectType: "Group Project",
-            projectResponsibility: "Was primarily responsible for setting up the functionality of our MongoDB/node backend and creating our profile manager page",
-            projectButtonLink: "https://spark-i-techs.herokuapp.com/",
-            githubButtonLink: "https://github.com/avlegend/Project3",
+            projectTitle: "Bamazon SQL Storefront",
+            projectImg: OnlineShoppingIcon,
+            projectDescription: "an app that allows users to operate a virtual storefront through node on the command line. ",
+            projectType: "Solo Project",
+            projectResponsibility: "Built the project in its entirety",
+            projectButtonLink: "https://github.com/BrandonMGH/MySQL_StoreFront_Database-",
+            githubButtonLink: "https://github.com/BrandonMGH/MySQL_StoreFront_Database-",
         },
         {
             keyValue: 4,
-            projectTitle: "CodeSpot",
-            projectImg: codespot,
-            projectDescription: "A MVC forum website designed for Web Developers who code using the MERN stack",
-            projectType: "Group Project",
-            projectResponsibility: "Primarily responsible, along with one other gentleman, with setting up our SQL/Node backend to set up the routes that handle user message inputs",
-            projectButtonLink: "https://codespot.herokuapp.com/",
-            githubButtonLink: "https://github.com/BrandonMGH/Project2"
-
-        },
-        {
-            keyValue: 5,
             projectTitle: "Friend-Finder",
             projectImg: bestfriends,
             projectDescription: "An app designed to match you up with your ideal friend.",
@@ -131,16 +120,27 @@ const sectionObject = {
             projectResponsibility: "Built the project in its entirety",
             projectButtonLink: "https://friend-finder-app-ucsd.herokuapp.com/home.html",
             githubButtonLink: "https://github.com/BrandonMGH/Friend-Finder"
+
+        },
+        {
+            keyValue: 5,
+            projectTitle: "WANT-A-BURGER?!",
+            projectImg: hamburger,
+            projectDescription: "an app that allows users to create create and eat their own burger.",
+            projectType: "Solo Project",
+            projectResponsibility: "Built the project in its entirety",
+            projectButtonLink: "https://want-a-burger.herokuapp.com/",
+            githubButtonLink: "https://github.com/BrandonMGH/Burger"
         },
         {
             keyValue: 6,
-            projectTitle: "Train Scheduler",
-            projectImg: train,
-            projectDescription: "An app that allows the user to enter a train, it's destination and its arrival time into the train arrival schedule",
-            projectType: "Solo Project",
-            projectResponsibility: "Built the project in its entirety",
-            projectButtonLink: "https://brandonmgh.github.io/Firebase-Assignment---Train-Scheduler/",
-            githubButtonLink: "https://github.com/BrandonMGH/Firebase-Assignment---Train-Scheduler"
+            projectTitle: "CodeSpot",
+            projectImg: codespot,
+            projectDescription: "A MVC forum website designed for Web Developers who code using the MERN stack",
+            projectType: "Group Project",
+            projectResponsibility: "Primarily responsible, along with one other gentleman, with setting up our SQL/Node backend to set up the routes that handle user message inputs",
+            projectButtonLink: "https://codespot.herokuapp.com/",
+            githubButtonLink: "https://github.com/BrandonMGH/Project2"
         }
     ],
     React: [
@@ -175,37 +175,6 @@ const sectionObject = {
             projectButtonLink: "https://spark-i-techs.herokuapp.com/",
             githubButtonLink: "https://github.com/avlegend/Project3",
         },
-        {
-            keyValue: 4,
-            projectTitle: "CodeSpot",
-            projectImg: codespot,
-            projectDescription: "A MVC forum website designed for Web Developers who code using the MERN stack",
-            projectType: "Group Project",
-            projectResponsibility: "Primarily responsible, along with one other gentleman, with setting up our SQL/Node backend to set up the routes that handle user message inputs",
-            projectButtonLink: "https://codespot.herokuapp.com/",
-            githubButtonLink: "https://github.com/BrandonMGH/Project2"
-
-        },
-        {
-            keyValue: 5,
-            projectTitle: "Friend-Finder",
-            projectImg: bestfriends,
-            projectDescription: "An app designed to match you up with your ideal friend.",
-            projectType: "Solo Project",
-            projectResponsibility: "Built the project in its entirety",
-            projectButtonLink: "https://friend-finder-app-ucsd.herokuapp.com/home.html",
-            githubButtonLink: "https://github.com/BrandonMGH/Friend-Finder"
-        },
-        {
-            keyValue: 6,
-            projectTitle: "Train Scheduler",
-            projectImg: train,
-            projectDescription: "An app that allows the user to enter a train, it's destination and its arrival time into the train arrival schedule",
-            projectType: "Solo Project",
-            projectResponsibility: "Built the project in its entirety",
-            projectButtonLink: "https://brandonmgh.github.io/Firebase-Assignment---Train-Scheduler/",
-            githubButtonLink: "https://github.com/BrandonMGH/Firebase-Assignment---Train-Scheduler"
-        }
     ],
     All: [
         {
