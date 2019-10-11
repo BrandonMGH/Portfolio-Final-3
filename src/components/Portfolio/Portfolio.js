d78954e4297df95072a9bf2173dccc397ca47e7b
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-10 17:38:48 -0700

    added shake effect animation to portfolio tile buttons

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -37,15 +37,28 @@ export default function Portfolio() {
     justify-content: center; 
     align-items: center; 
     opacity: .7;
-    transition:  transform 2s;
     width: 350px
+    @keyframes shake {
+        0% { transform: translate(1px, 1px) rotate(0deg); }
+        10% { transform: translate(-1px, -2px) rotate(-1deg); }
+        20% { transform: translate(-3px, 0px) rotate(1deg); }
+        30% { transform: translate(3px, 2px) rotate(0deg); }
+        40% { transform: translate(1px, -1px) rotate(1deg); }
+        50% { transform: translate(-1px, 2px) rotate(-1deg); }
+        60% { transform: translate(-3px, 1px) rotate(0deg); }
+        70% { transform: translate(3px, 1px) rotate(-1deg); }
+        80% { transform: translate(-1px, -1px) rotate(1deg); }
+        90% { transform: translate(1px, 2px) rotate(0deg); }
+        100% { transform: translate(1px, -2px) rotate(-1deg); }
+      }
    `
     const TileJavascript = styled(PortfolioTile)`
     :hover {
         border: 5px solid orange; 
         color: orange
         cursor: pointer; 
-        transform: rotate(360deg);
+        animation: shake 2s;
+        animation-iteration-count: infinite;
       }
    `
     const TileNode = styled(PortfolioTile)`
@@ -53,7 +66,8 @@ export default function Portfolio() {
         border: 5px solid green; 
         color: green
         cursor: pointer; 
-        transform: rotate(360deg);
+        animation: shake 2s;
+        animation-iteration-count: infinite;
       }
    `
     const TileReact = styled(PortfolioTile)`
@@ -61,7 +75,8 @@ export default function Portfolio() {
         border: 5px solid blue; 
         color: blue
         cursor: pointer; 
-        transform: rotate(360deg);
+        animation: shake 2s;
+        animation-iteration-count: infinite;
       }
    `
     const TileAll = styled(PortfolioTile)`
@@ -69,7 +84,8 @@ export default function Portfolio() {
         border: 5px solid red; 
         color: red
         cursor: pointer; 
-        transform: rotate(360deg);
+        animation: shake 2s;
+        animation-iteration-count: infinite;
       }
    `
     const TileText = styled.h4`
