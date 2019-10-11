f80526b829b4050e627ff89e37e868159dc3e2b0
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-11 14:58:40 -0700

    working on getting the contact icon images to size properly within the grid

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import ContactMeObject from './ContactMeObject.js'
 export default function ContactMe() {
+
+    const ContactWrapper = styled.section`
+    display: grid
+    grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+    grid-gap: 1rem;
+    place-items: center; 
+    `
+
+
+    const IconWrapper = styled.section`
+   object-fit:cover 
+    `
+
     return (
-        <div>
-            <h1>Contact Me</h1>
-        </div>
+        <ContactWrapper>
+            {ContactMeObject.Arr.map(properties => (
+                <a href={properties.link}>
+                    <IconWrapper>
+                        <img src={properties.image}></img>
+                    </IconWrapper>
+                </a>
+            ))}
+        </ContactWrapper>
     )
 }
---
+++ src/components/ContactMe/ContactMeObject.js
@@ -0,0 +1,34 @@
+import GitHubMark from './Images/GitHubMark.png'
+import linkedinlogo from './Images/linkedinlogo.png'
+import EmailLogofrom from './Images/EmailLogo.png'
+import styled from 'styled-components'
+
+const ImageWrapper = styled.section`
+    height: 350px;
+    Weight: 350px;  
+    `
+
+const ContactMeObject = {
+    Arr:[ 
+        {
+        image: linkedinlogo,
+        link: "https://www.linkedin.com/in/brandon-harris-471bb932/",
+        text: "LinkedIn",
+    },
+    {
+        image: EmailLogofrom,
+        link: "mailto:BrandonMGH@Yahoo.com?Subject=Portfolio Email Message",
+        text: "Email",
+
+    },
+    {
+        image: GitHubMark,
+        link: "https://github.com/BrandonMGH",
+        text: "Github",
+
+    },
+]
+  
+}
+
+export default ContactMeObject
