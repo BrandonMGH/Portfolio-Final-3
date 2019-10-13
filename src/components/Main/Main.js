e10c409008b59394b41cce94e201337d0bc0fd43
BrandonMGH <BrandonMGH@Yahoo.com>
2019-10-12 21:02:41 -0700

    added styling to navbar and got the navbar functionality up and running

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -11,14 +11,23 @@ import Footer from '../Footer/Footer.js'
 
 
 export default function Main() {
+
+    const scrollIntoViewInfo = {
+        Title: "TitleScroll",
+        About: "AboutMeScroll",
+        Skills: "SkillsScroll",
+        Portfolio: "PortfolioScroll",
+        Contact: "ContactMeScroll"
+      }
+
     return (
         <div>
-           <NavigationBar />
-           <Title />
-           <AboutMe />
-           <Skills />
-           <Portfolio />
-           <ContactMe />
+           <div><NavigationBar /></div>
+           <div className={scrollIntoViewInfo.Title}><Title /></div>
+           <div className={scrollIntoViewInfo.About}><AboutMe /></div>
+           <div className={scrollIntoViewInfo.Skills}><Skills /></div>
+           <div className={scrollIntoViewInfo.Portfolio}><Portfolio /></div>
+           <div className={scrollIntoViewInfo.Contact}><ContactMe /></div>
            <Footer />
 
         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,20 +3,33 @@ import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstr
 import ScrollIntoView from 'react-scroll-into-view'
 
 export default function NavigationBar() {
+
+    const fixed = {
+        position: `fixed`,
+        width: `100vw`,
+        
+    }
+
+    const textColor = {
+
+        color: `white`
+    }
+
+
     return (
-        <div className="fixed"> 
+        <div style={fixed}> 
             <Navbar bg="dark" className="color"expand="lg" >
-                <Navbar.Brand ><span className="textColorPrimary">My Portfolio</span></Navbar.Brand>
+                <Navbar.Brand ><span style={textColor}>My Portfolio</span></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                         <ScrollIntoView selector=".AboutMeScroll"><Nav.Link><span className="textColor">About Me</span></Nav.Link></ScrollIntoView>
-                         <ScrollIntoView selector=".SkillsScroll"><Nav.Link><span className="textColor">Skills</span></Nav.Link></ScrollIntoView>
-                         <ScrollIntoView selector=".PortfolioScroll"><Nav.Link><span className="textColor">Portfolio</span></Nav.Link></ScrollIntoView>
-                         <ScrollIntoView selector=".ContactMeScroll"><Nav.Link><span className="textColor">Contact Me</span></Nav.Link></ScrollIntoView>
+                         <ScrollIntoView selector=".AboutMeScroll"><Nav.Link><span style={textColor}>About Me</span></Nav.Link></ScrollIntoView>
+                         <ScrollIntoView selector=".SkillsScroll"><Nav.Link><span style={textColor}>Skills</span></Nav.Link></ScrollIntoView>
+                         <ScrollIntoView selector=".PortfolioScroll"><Nav.Link><span style={textColor}>Portfolio</span></Nav.Link></ScrollIntoView>
+                         <ScrollIntoView selector=".ContactMeScroll"><Nav.Link><span style={textColor}>Contact Me</span></Nav.Link></ScrollIntoView>
                     </Nav>
                     <Form inline>
-                    <ScrollIntoView selector=".TitleScroll"><Nav.Link><span className="textColor">Back to the Top</span></Nav.Link></ScrollIntoView>
+                    <ScrollIntoView selector=".TitleScroll"><Nav.Link><span style={textColor}>Back to the Top</span></Nav.Link></ScrollIntoView>
                     </Form>
                 </Navbar.Collapse>
             </Navbar>
